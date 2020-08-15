d74143788d9cd30834df6e7699b8606c6651951a
pujansoni <pujansoni1997@gmail.com>
2020-08-15 02:35:07 -0400

    Adding trydjango project

---
+++ trydjango/pages/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    print(request.user)
+    return render(request, "home.html", {})
