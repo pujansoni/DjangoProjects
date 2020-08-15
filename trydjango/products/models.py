b5b30a30222c322192fa4298a37bf47430252e24
pujansoni <pujansoni1997@gmail.com>
2020-08-15 07:16:03 -0400

    reverse method namespace

--- trydjango/products/models.py
+++ trydjango/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -8,3 +9,6 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=10000)
     summary = models.TextField(blank=False, null=False)
     featured = models.BooleanField(default=False) # null=True default=True
+
+    def get_absolute_url(self):
+        return reverse("products:product-detail", kwargs={"id": self.id})  # f"/products/{self.id}/"
