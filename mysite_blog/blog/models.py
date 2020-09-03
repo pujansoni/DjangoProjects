93f01a28a1b0cd68ae9d3287a287651da2f4a630
pujansoni <pujansoni1997@gmail.com>
2020-09-03 17:53:38 -0400

    Adding the class based views and pagination for listing posts

--- mysite_blog/blog/models.py
+++ mysite_blog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -14,8 +15,6 @@ class Post(models.Model):
         ('draft', 'Draft'),
         ('published', 'Published')
     )
-    objects = models.Manager()
-    published = PublishedManager()
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
@@ -24,9 +23,17 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
             return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
