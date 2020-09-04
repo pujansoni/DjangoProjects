8c71c4469614b850d4d1eb872c3233c68b4cd6d8
pujansoni <pujansoni1997@gmail.com>
2020-09-03 23:12:38 -0400

    Adding email feature on each blog post

--- mysite_blog/mysite_blog/settings.py
+++ mysite_blog/mysite_blog/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -119,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'pujansoniwindsor@gmail.com'
+EMAIL_HOST_PASSWORD = 'pujan@windsor.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
