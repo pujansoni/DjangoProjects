d8d9c8e2a4834d1723eb78c21fc4ea339ab27196
pujansoni <pujansoni1997@gmail.com>
2020-09-17 17:58:33 -0400

    Adding SearchVector, SearchQuery, and SearchRank for posts

--- mysite_blog/blog/views.py
+++ mysite_blog/blog/views.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Post, Comment
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 from django.db.models import Count
@@ -45,7 +45,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            # results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)
+            #                                   ).filter(search=search_query).order_by('-rank')
+            results = Post.published.annotate(rank=SearchRank(search_vector, search_query)
+                                              ).filter(rank__gte=0.3).order_by('-rank')
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
 
 
