c86184f7a721e35aa2d8e552d5594b2dde59a084
pujansoni <pujansoni1997@gmail.com>
2020-09-18 17:09:34 -0400

    Adding change password functionality using Django default PasswordChangeView and PasswordChangeDoneView

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    # change password urls
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
