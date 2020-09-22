8ddad49871cb4e2c8db07a2876c6ba70169f104e
pujansoni <pujansoni1997@gmail.com>
2020-09-21 23:43:42 -0400

    Adding ipdb instruction file and extending the user model functionality for bookmarks project

--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
 ]
+
+# The static() helper function is suitable for development, but not for production use. Django is very inefficient at
+# serving static files. Never serve your static files with Django in a production environment.
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
