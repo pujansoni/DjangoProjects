1898c6a5ce49afe164249aa32b13c553e3f1969d
pujansoni <pujansoni1997@gmail.com>
2020-10-24 23:09:46 -0400

    Adding base.html file for inheritance and changing detail.html, index.html, and about.html templates for mysite20 project

--- mysite20/myapp/views.py
+++ mysite20/myapp/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import get_object_or_404, render
 # Create your views here.
 def index(request):
     top_list = Topic.objects.all().order_by('id')[:10]
-    return render(request, 'myapp/index0.html', {'top_list': top_list})
+    return render(request, 'myapp/index.html', {'top_list': top_list})
     # course_list = Course.objects.all().order_by('-title')[:5]
     # response = HttpResponse()
     # heading1 = '<p>' + 'List of topics: ' + '</p>'
@@ -24,14 +24,14 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'myapp/about0.html')
+    return render(request, 'myapp/about.html')
     # return HttpResponse("This is an E-learning Website! Search our Topics to find all available Courses.")
 
 
 def detail(request, topic_id):
     topic = get_object_or_404(Topic, id=topic_id)
     # topic = Topic.objects.get(id=topic_id)
-    return render(request, 'myapp/detail0.html', {'topic': topic})
+    return render(request, 'myapp/detail.html', {'topic': topic})
     # response = HttpResponse()
     # heading1 = '<p>' + str(topic.name.upper()) + '</p>'
     # response.write(heading1)
