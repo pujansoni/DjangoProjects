1dd2ff9958e146c1d81d1c164143605a06d59121
pujansoni <pujansoni1997@gmail.com>
2020-12-01 01:43:58 -0500

    Manipulating Topic model for admin interface in mysite20

--- mysite20/myapp/admin.py
+++ mysite20/myapp/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Topic, Course, Student, Order, Review
 
 
+class CourseInline(admin.TabularInline):
+    model = Course
+
+
 class CourseAdmin(admin.ModelAdmin):
     fields = [('title', 'topic'), ('price', 'num_reviews', 'for_everyone')]
     list_display = ('title', 'topic', 'price')
@@ -12,8 +16,13 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'student', 'order_status', 'order_date', 'courses_ordered')
 
 
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name', 'length')
+    inlines = [CourseInline,]
+
+
 # Register your models here.
-admin.site.register(Topic)
+admin.site.register(Topic, TopicAdmin)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Student)
 admin.site.register(Order, OrderAdmin)
