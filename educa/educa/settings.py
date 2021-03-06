089a9b96b4389134811b76525e9329942103a8fa
pujansoni <pujansoni1997@gmail.com>
2021-03-05 23:17:52 -0500

    Adding async chat server to educa project

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -151,3 +151,12 @@ REST_FRAMEWORK = {
 }
 
 ASGI_APPLICATION = 'educa.routing.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
